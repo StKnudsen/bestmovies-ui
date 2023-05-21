4efa30cc229beb400447b9a74150d6e7ef273878
Stefan Knudsen <st.knudsen@gmail.com>
2023-05-21 18:01:44 +0200

    removed config.ts from .gitignore

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+export const config = {
+  BFF: "https://bestmovies-bff.azurewebsites.net/",
+  IMG_URL: "https://image.tmdb.org/t/p/w500/",
+  firebase: {
+    apiKey: "AIzaSyDYJ8M5iWk7-sx6mAUNFCtv_zNR8OyxlcE",
+    authDomain: "bestmovies-bf7fd.firebaseapp.com",
+    projectId: "bestmovies-bf7fd",
+    storageBucket: "bestmovies-bf7fd.appspot.com",
+    messagingSenderId: "864516772402",
+    appId: "1:864516772402:web:999c47a4ea1e8fca48e22b",
+  },
+};
